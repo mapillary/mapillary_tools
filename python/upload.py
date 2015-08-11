2bd9dae85718c5683076ca7ad9d3da1d31fcfc07
Tomas Janousek <tomi@nomi.cz>
2015-08-11 08:13:29 +0200

    Make NUMBER_THREADS/MAX_ATTEMPTS configurable from env

--- python/upload.py
+++ python/upload.py
@@ -33,8 +33,8 @@ MAPILLARY_UPLOAD_URL = "https://d22zcsn13kp53w.cloudfront.net/"
 PERMISSION_HASH = "eyJleHBpcmF0aW9uIjoiMjAyMC0wMS0wMVQwMDowMDowMFoiLCJjb25kaXRpb25zIjpbeyJidWNrZXQiOiJtYXBpbGxhcnkudXBsb2Fkcy5pbWFnZXMifSxbInN0YXJ0cy13aXRoIiwiJGtleSIsIiJdLHsiYWNsIjoicHJpdmF0ZSJ9LFsic3RhcnRzLXdpdGgiLCIkQ29udGVudC1UeXBlIiwiIl0sWyJjb250ZW50LWxlbmd0aC1yYW5nZSIsMCwyMDQ4NTc2MF1dfQ=="
 SIGNATURE_HASH = "f6MHj3JdEq8xQ/CmxOOS7LvMxoI="
 BOUNDARY_CHARS = string.digits + string.ascii_letters
-NUMBER_THREADS = 4
-MAX_ATTEMPTS = 10
+NUMBER_THREADS = int(os.getenv('NUMBER_THREADS', '4'))
+MAX_ATTEMPTS = int(os.getenv('MAX_ATTEMPTS', '10'))
 UPLOAD_PARAMS = {"url": MAPILLARY_UPLOAD_URL, "permission": PERMISSION_HASH, "signature": SIGNATURE_HASH, "move_files":True}
 
 
