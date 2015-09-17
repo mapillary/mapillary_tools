d86ac7d3e7c765df672465a5bc65e07fd6cbeb5b
Yubin Kuang <yubin.kse@gmail.com>
2015-09-17 09:53:22 +0200

    Add @simonmikkelsens duplicate detection to sequence

--- python/lib/sequence.py
+++ python/lib/sequence.py
@@ -8,8 +8,7 @@ from lib.exif import EXIF
 Sequence class for organizing/cleaning up photos in a folder
     - split to sequences based on time intervals
     - split to sequences based on gps distances
-    - remove duplicate images (e.g. waiting for red light, in traffic etc)
-@contributors:
+    - remove duplicate images (e.g. waiting for red light, in traffic etc) @simonmikkelsen
 '''
 
 class Sequence(object):
@@ -18,42 +17,66 @@ class Sequence(object):
         self.filepath = filepath
         self.get_file_list(filepath)
 
-    def get_file_list(self, filepath):
-        file_list = []
-        for root, sub_folders, files in os.walk(self.filepath):
-            file_list += [os.path.join(root, filename) for filename in files if filename.lower().endswith(".jpg")]
-        self.file_list = file_list
-        return file_list
+    def _is_skip(self, filepath):
+        '''
+        Skip photos in specified folders
+            - filepath/duplicates: it stores potential duplicate photos
+                                   detected by method 'remove_duplicates`
+        '''
+        _is_skip = True if 'duplicates' in filepath else False
+        return _is_skip
 
-    def read_capture_time(self, filename):
+    def _read_capture_time(self, filename):
         '''
         Use EXIF class to parse capture time from EXIF.
         '''
         exif = EXIF(filename)
         return exif.extract_capture_time()
 
-    def read_lat_lon(self, filename):
+    def _read_lat_lon(self, filename):
+        '''
+        Use EXIF class to parse latitude and longitude from EXIF.
+        '''
         exif = EXIF(filename)
         lon, lat = exif.extract_lon_lat()
         return lat, lon
+
+    def _read_direction(self, filename):
+        '''
+        Use EXIF class to parse compass direction from EXIF.
+        '''
+        exif = EXIF(filename)
+        direction = exif.extract_direction()
+        return direction
+
+    def get_file_list(self, filepath):
+        '''
+        Get the list of JPEGs in the folder (nested folders)
+        '''
+        file_list = []
+        for root, sub_folders, files in os.walk(self.filepath):
+            if not self._is_skip(root):
+                file_list += [os.path.join(root, filename) for filename in files
+                                if (filename.lower().endswith(".jpg"))]
+        self.file_list = file_list
+        return file_list
 
     def sort_file_list(self, file_list):
         '''
         Read capture times and sort files in time order.
         '''
-        capture_times = [self.read_capture_time(filepath) for filepath in file_list]
+        capture_times = [self._read_capture_time(filepath) for filepath in file_list]
         sorted_times_files = zip(capture_times, file_list)
         sorted_times_files.sort()
         return zip(*sorted_times_files)
 
-    def move_groups(self, groups):
+    def move_groups(self, groups, sub_path=''):
         '''
         Move the files in the groups to new folders.
         '''
-        print("Organizing into {0} groups.".format(len(groups)))
         for i,group in enumerate(groups):
-            group_path = os.path.dirname(group[0])
-            new_dir = os.path.join(group_path, str(i))
+            # group_path = os.path.dirname(group[0])
+            new_dir = os.path.join(self.filepath, sub_path, str(i))
             lib.io.mkdir_p(new_dir)
             for filepath in group:
                 os.rename(filepath, os.path.join(new_dir, os.path.basename(filepath)))
@@ -75,7 +98,7 @@ class Sequence(object):
         capture_deltas = [t2-t1 for t1,t2 in zip(capture_times, capture_times[1:])]
 
         # read gps for ordered files
-        latlons = [self.read_lat_lon(filepath) for filepath in file_list]
+        latlons = [self._read_lat_lon(filepath) for filepath in file_list]
 
         # distance between consecutive images
         distances = [lib.geo.gps_distance(ll1, ll2) for ll1, ll2 in zip(latlons, latlons[1:])]
@@ -126,7 +149,7 @@ class Sequence(object):
             capture_times, file_list = self.sort_file_list(file_list)
 
             # read gps for ordered files
-            latlons = [self.read_lat_lon(filepath) for filepath in file_list]
+            latlons = [self._read_lat_lon(filepath) for filepath in file_list]
 
             if len(file_list)>1:
                 # bearing between consecutive images
@@ -140,3 +163,56 @@ class Sequence(object):
             bearings = {file_list[0]: lib.geo.offset_bearing(0.0, offset)}
 
         return bearings
+
+    def remove_duplicates(self, min_distance=1e-5, min_angle=5):
+        '''
+        Detect duplidate photos in a folder
+        @source:  a less general version of @simonmikkelsen's duplicate remover
+        '''
+        file_list = self.file_list
+
+        # ordered list by time
+        capture_times, file_list = self.sort_file_list(file_list)
+
+        # read gps for ordered files
+        latlons = [self._read_lat_lon(filepath) for filepath in file_list]
+
+        # read bearing for ordered files
+        bearings = [self._read_direction(filepath) for filepath in file_list]
+
+        is_duplicate = False
+
+        prev_unique = file_list[0]
+        prev_latlon = latlons[0]
+        prev_bearing = bearings[0]
+        groups = []
+        group = []
+        for i, filename in enumerate(file_list[1:]):
+            k = i+1
+            distance = lib.geo.gps_distance(latlons[k], prev_latlon)
+            if bearings[i] is not None and prev_bearing is not None:
+                bearing_diff = lib.geo.diff_bearing(bearings[k], prev_bearing)
+            else:
+                # Not use bearing difference if no bearings are available
+                bearing_diff = 360
+            if distance < min_distance and bearing_diff < min_angle:
+                is_duplicate = True
+            else:
+                prev_latlon = latlons[k]
+                prev_bearing = bearings[k]
+
+            if is_duplicate:
+                group.append(filename)
+            else:
+                if group:
+                    groups.append(group)
+                group = []
+
+            is_duplicate = False
+
+        # move to filepath/duplicates/group_id (TODO: uploader should skip the duplicate folder)
+        self.move_groups(groups, 'duplicates')
+        print("Done remove duplicate photos in {} into {} groups".format(self.filepath, len(groups)))
+
+        return groups
+
