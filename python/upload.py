d74ac228f9da3a4d916df1be61087edd77f43bab
Adam Jones <jones_adam@rocketmail.com>
2016-03-16 19:27:42 +0000

    Update upload.py
    Add --delete_after_upload option

--- python/upload.py
+++ python/upload.py
@@ -23,15 +23,16 @@ FILES WILL BE IGNORED SERVER-SIDE.
 
 if __name__ == '__main__':
     '''
-    Usage: python upload.py [--upload_subfolders] path
+    Usage: python upload.py [--upload_subfolders] [--delete_after_upload] path
     '''
 
     if sys.version_info >= (3, 0):
         raise IOError("Incompatible Python version. This script requires Python 2.x, you are using {0}.".format(sys.version_info[:2]))
 
     parser = argparse.ArgumentParser(description='Upload photos taken with Mapillary apps')
-    parser.add_argument('path', help='path to your photos')
+    parser.add_argument('path', help='path to your photos from the Mapillary app')
     parser.add_argument('--upload_subfolders', help='option to upload subfolders', action='store_true')
+    parser.add_argument('--delete_after_upload', help='option to delete images after they have been successfully uploaded', action='store_true')
     args = parser.parse_args()
 
     path = args.path
@@ -42,6 +43,12 @@ if __name__ == '__main__':
     file_list = [f for f in s.file_list if verify_mapillary_tag(f)]
 
     print ("Uploading {} images with valid mapillary tags (Skipping {})".format(len(file_list), num_image_file-len(file_list)))
-    upload_file_list(file_list)
+
+    if args.delete_after_upload is True:
+        delete_after_upload = 1;
+    else:
+        delete_after_upload = 0;
+
+    upload_file_list(file_list, delete_after_upload)
 
     print("Done uploading {} images.".format(len(file_list)))
