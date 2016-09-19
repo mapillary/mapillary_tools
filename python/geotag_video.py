7b278fe14c78a8eb6a73e9f4a4036dc148f44fd8
Yubin Kuang <yubin.kse@gmail.com>
2016-09-19 14:05:53 +0200

    Add support for nmea trace in geotagging video

--- python/geotag_video.py
+++ python/geotag_video.py
@@ -7,10 +7,11 @@ import json
 import lib.io as io
 import lib.exifedit as exifedit
 import lib.geo as geo
-from geotag_from_gpx import get_lat_lon_time
-from ffprobe import FFProbe
+from lib.gps_parser import get_lat_lon_time_from_gpx, get_lat_lon_time_from_nmea
+from lib.ffprobe import FFProbe
 
 ZERO_PADDING = 6
+TIME_FORMAT = "%Y-%m-%d %H:%M:%S"
 
 def sample_video(video_file, image_path, sample_interval):
     """Sample video frame with the specified time interval
@@ -23,18 +24,40 @@ def sample_video(video_file, image_path, sample_interval):
     s = "ffmpeg -i {} -loglevel quiet -vf fps=1/{} {}/%0{}d.jpg".format(video_path, sample_interval, image_path, ZERO_PADDING)
     os.system(s)
 
+
 def get_video_duration(video_file):
     """Get video duration in seconds"""
     return float(FFProbe(video_file).video[0].duration)
 
+
+def get_video_start_time(video_file):
+    """Get video duration in seconds"""
+    time_string = FFProbe(video_file).video[0].creation_time
+    creation_time = datetime.datetime.strptime(time_string, TIME_FORMAT)
+    return creation_time
+
+
+def parse_gps_trace(gps_trace_file, local_time=False):
+    points = None
+    if gps_trace_file.lower().endswith(".gpx"):
+        points = get_lat_lon_time_from_gpx(gps_trace_file, local_time)
+    elif gps_trace_file.lower().endswith(".nmea"):
+        points = get_lat_lon_time_from_nmea(gps_trace_file, local_time)
+    elif gps_trace_file.lower().endswith(".srt"):
+        pass
+    return points
+
+
 def list_images(image_path):
     return [s for s in os.listdir(image_path) if s.lower().endswith(".jpg")]
+
 
 def timestamp_from_filename(filename, sample_interval, start_time, video_duration, offset=0):
     seconds = (int(filename.lstrip("0").rstrip(".jpg"))-1)*sample_interval
     if seconds > video_duration:
         seconds = video_duration
     return start_time + datetime.timedelta(seconds=seconds)
+
 
 def get_args():
     p = argparse.ArgumentParser(description='Sample and geotag video with location and orientation from GPX file.')
@@ -43,7 +66,9 @@ def get_args():
     p.add_argument('--sample_interval', help='Time interval for sampled frames in seconds', default=2, type=float)
     p.add_argument('--gps_trace', help='GPS track file')
     p.add_argument('--time_offset', help='Time offset between video and gpx file in seconds', default=0, type=float)
+    p.add_argument("--skip_sampling", help="Skip video sampling step", action="store_true")
     return p.parse_args()
+
 
 if __name__ == "__main__":
 
@@ -55,19 +80,30 @@ if __name__ == "__main__":
     time_offset = args.time_offset
 
     # Parse gps trace
-    points = get_lat_lon_time(gps_trace_file)
-    start_time = points[0][0] + datetime.timedelta(seconds=time_offset)
+    local_time = False
+    points = parse_gps_trace(gps_trace_file, local_time)
+
+    # Get sync between video and gps trace
+    start_time = get_video_start_time(video_path)
+    start_time = None
+    if start_time is None:
+        start_time = points[0][0]
+    start_time += datetime.timedelta(seconds=time_offset)
+    print "Video starts at : {}".format(start_time)
+    print "GPS trace starts at: {}".format(points[0][0])
 
     # Get duration of the video
     video_duration = get_video_duration(video_path)
     gps_duration = (points[-1][0] - points[0][0])
 
     # Sample video
-    sample_video(video_path, image_path, sample_interval)
-
-    image_list = list_images(image_path)
+    if not args.skip_sampling:
+        sample_video(video_path, image_path, sample_interval)
 
     # Add EXIF data to sample images
+    image_list = list_images(image_path)
+    print "Adding EXIF to {} images".format(len(image_list))
+
     missing_gps = 0
     for i, im in enumerate(image_list):
         io.progress(i, len(image_list))
@@ -76,6 +112,8 @@ if __name__ == "__main__":
                                             start_time,
                                             video_duration,
                                             time_offset)
+        print timestamp
+
         try:
             lat, lon, bearing, altitude = geo.interpolate_lat_lon(points, timestamp)
             data = {
@@ -86,8 +124,9 @@ if __name__ == "__main__":
                 "bearing": bearing
             }
             exifedit.add_exif_data(os.path.join(image_path, im), data)
-        except:
+        except Exception as e:
+            print e
             print "Image {} timestamp out of range. Skipping".format(im)
             missing_gps += 1
 
-    print "{} images missing gps".format(missing_gps)
+    print "{} image samples with {} images missing gps".format(len(image_list), missing_gps)
