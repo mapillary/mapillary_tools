66323edbe495b073a800d102d0b01d9c09c68433
Yubin Kuang <yubin.kse@gmail.com>
2017-10-01 02:13:47 +0200

    feat(json): Add status check and options

--- python/add_mapillary_tag_from_json.py
+++ python/add_mapillary_tag_from_json.py
@@ -9,7 +9,7 @@ import argparse
 
 from lib.uploader import get_upload_token, get_authentication_info, get_project_key
 from lib.exifedit import add_mapillary_description
-from lib.io import progress
+from lib.io import progress, mkdir_p
 
 '''
 Script for reading the meta data for images from a json file and create the
@@ -19,11 +19,14 @@ needed to be able to upload without authentication.
 
 def get_args():
     p = argparse.ArgumentParser(description='Geotag one or more photos with location and orientation from GPX file.')
-    p.add_argument('file_path', help='File path to the data file that contains the metadata')
-    p.add_argument('--json_path', help='Path to json file that contains meta data.')
-    p.add_argument('--output_path', help='Output path for image files')
+    p.add_argument('file_path', help='File path to the images')
+    p.add_argument('--json_path', help='Path to json file that contains metadata.')
+    p.add_argument('--output_path', help='Output path for image files', default="")
     p.add_argument('--project', help='Project name at Mapillary.')
+    p.add_argument('--project_key', help='Project name at Mapillary.')
     p.add_argument('--skip_authentication', help='Skip authentication', action="store_true")
+    p.add_argument('--skip_validate_project', help='Skip validating project', action="store_true")
+
     return p.parse_args()
 
 if __name__ == '__main__':
@@ -32,7 +35,12 @@ if __name__ == '__main__':
     '''
 
     args = get_args()
-    project_key = args.project
+    # Retrieve/validate project key
+    if not args.skip_validate_project:
+        project_key = get_project_key(args.project, args.project_key)
+    else:
+        project_key = args.project_key or ''
+
     image_path = args.file_path
     json_path = args.json_path if args.json_path is not None else os.path.join(image_path, "mapillary_tag.json")
     output_path = args.output_path
@@ -47,7 +55,6 @@ if __name__ == '__main__':
             sys.exit()
 
         upload_token = get_upload_token(MAPILLARY_EMAIL, MAPILLARY_PASSWORD)
-        project_key = get_project_key(project_key)
     else:
         MAPILLARY_USERNAME, MAPILLARY_EMAIL, MAPILLARY_PASSWORD = None, None, None
         upload_token = None
@@ -55,10 +62,7 @@ if __name__ == '__main__':
     with open(json_path, "rb") as f:
         metadata = json.loads(f.read())
 
-    if output_path is not None and os.path.exists(output_path) is False:
-        os.makedirs(output_path)
-    else:
-        output_path = ""
+    mkdir_p(output_path)
 
     images = os.listdir(image_path)
     for i, f in enumerate(images):
@@ -78,3 +82,6 @@ if __name__ == '__main__':
             progress(i, len(images))
         else:
             print "Ignoring {0}".format(os.path.join(image_path, f))
+
+    with open(os.path.join(output_path, "EXIF_DONE"), "wb") as f:
+        f.write("")
