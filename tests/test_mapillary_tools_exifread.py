3784fcdadcc716c205254537a8704fbbca397d5e
jernejaMislej <jerneja.mislej@gmail.com>
2017-10-24 12:09:08 +0200

    create tests folder with a test script and an images folder, which contains a sample image taken from piexif tests

---
+++ tests/test_mapillary_tools_exifread.py
@@ -0,0 +1,14 @@
+import os
+import sys
+import unittest
+from PIL import Image
+import piexif
+
+print("piexif version: {0}".format(piexif.VERSION))
+
+class ExifTests(unittest.TestCase):
+    """tests for main functions."""
+    
+    def test_if_this_works(self):
+        pass
+
