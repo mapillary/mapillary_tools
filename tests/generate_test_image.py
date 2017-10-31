4a8cd825a0f9e2c3c7eda68c2276b3894e6cdf58
Oscar Lorentzon <oscar.lorentzon@gmail.com>
2017-10-31 14:59:46 +0000

    fix: transplant exif to empty exif image
    Transplant from input image to loaded emtpy exif image to avoid
    creating and saving PIL image which requires JPEG encoder and
    PIL >=3.1.

--- tests/generate_test_image.py
+++ tests/generate_test_image.py
@@ -1,14 +1,13 @@
 import argparse
 import io
+import os
 import piexif
-
-from PIL import Image
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(description='Generate a test image by transplanting exif')
-    parser.add_argument('input_image', help='path to imput image containing exif')
-    parser.add_argument('output_image', help='path to generated output image')
+    parser = argparse.ArgumentParser(description="Generate a test image by transplanting exif")
+    parser.add_argument("input_image", help="path to imput image containing exif")
+    parser.add_argument("output_image", help="path to generated output image")
 
     return parser.parse_args()
 
@@ -19,8 +18,11 @@ if __name__ == "__main__":
     with open(args.input_image, "rb") as fin:
         input_string = fin.read()
 
-    Image.new("RGB", (16, 16), (255, 0, 0)).save(args.output_image)
-    with open(args.output_image, "rb") as f:
+    empty_image = os.path.join(
+        os.path.abspath(os.path.dirname(__file__)),
+        "data/empty_exif.jpg")
+
+    with open(empty_image, "rb") as f:
         image_string = f.read()
 
     output_bytes = io.BytesIO()
