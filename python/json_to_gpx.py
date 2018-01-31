4cf40bca231049e608411032e73c0a64f4a48459
jernejaMislej <jerneja.mislej@gmail.com>
2018-01-31 15:22:05 +0100

    feat: several missing features in the script
    the script is missing options to pass args for gpx trace file name, path
    and then save it to file instead of printing it out.
    also if the lat,lon or alt are not in json, which does actually happen,
    the script will fail, so there is a check missing there

--- python/json_to_gpx.py
+++ python/json_to_gpx.py
@@ -6,12 +6,19 @@ import os
 
 def get_args():
     import argparse
-    p = argparse.ArgumentParser(description='Convert Mapillary ios json format to gpx.')
-    p.add_argument('path', help='Path to folder of json files.')
+    p = argparse.ArgumentParser(description = 'Convert Mapillary ios json format to gpx.')
+    p.add_argument('path', help = 'Path to folder of json files.')
+    p.add_argument('-o', '--out_path', help = 'path to where the gpx trace should be stored', required = False, default = ".")
+    p.add_argument('-g', '--gpx_trace_name', help = 'name for the gpx trace', required = False, default = None)
     return p.parse_args()
 
 if __name__ == '__main__':
     args = get_args()
+
+    if args.gpx_trace_name == None:
+        gpx_full_file_name = os.path.join(args.out_path, os.path.basename(args.path) + ".gpx")
+    else:
+        gpx_full_file_name = os.path.join(args.out_path, args.gpx_trace_name + ".gpx")
 
     gpx = "<gpx>"
     gpx += "<trk>"
@@ -41,13 +48,16 @@ if __name__ == '__main__':
         if os.path.isfile(filename):
             with open(filename, "r") as f:
                 data = json.load(f)
-                gpx += "<trkpt lat=\"" + str(data['MAPLatitude']) + "\" lon=\"" + str(data['MAPLongitude']) + "\">"
-                gpx += "<ele>" + str(data['MAPAltitude']) + "</ele>"
-                gpx += "<time>" + t + "</time>"
-                gpx += "</trkpt>"
+                if ('MAPLatitude' in data) and ('MAPLongitude' in data):
+                    gpx += "<trkpt lat=\"" + str(data['MAPLatitude']) + "\" lon=\"" + str(data['MAPLongitude']) + "\">"
+                    if 'MAPAltitude' in data:
+                        gpx += "<ele>" + str(data['MAPAltitude']) + "</ele>"
+                    gpx += "<time>" + t + "</time>"
+                    gpx += "</trkpt>"
 
     gpx += "</trkseg>"
     gpx += "</trk>"
     gpx += "</gpx>"
 
-    print gpx
+    with open(gpx_full_file_name, "w") as f:
+        f.write(gpx)
