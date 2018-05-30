a7c81544978c79355b6646fd3f8751a3ddf124b2
jernejaMislej <jerneja.mislej@gmail.com>
2018-05-30 22:35:34 +0200

    fix: test specific requirement

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='mapillary_tools',
       version='0.0.0',
@@ -9,11 +9,11 @@ setup(name='mapillary_tools',
       license='BSD',
       packages=['mapillary_tools', 'mapillary_tools.commands'],
       scripts=['bin/mapillary_tools'],
+      setup_requires=["pytest-runner"],
       install_requires=[
           'exifread==1.4.1',
           'gpxpy==0.9.8',
-          'Pillow==2.9.0',
           'python-dateutil==2.7.3',
-          'pynmea2==1.12.0',
-          'pytest==3.2.3'
-      ])
+          'pynmea2==1.12.0'
+      ],
+      tests_require=['pytest==3.2.3', 'Pillow==2.9.0'])
