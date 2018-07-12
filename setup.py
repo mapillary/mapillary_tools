3bb1e199c52f637411cffad4f29574b5b7f57810
jernejaMislej <jerneja.mislej@gmail.com>
2018-07-13 00:03:07 +0200

    add: merge branch for v0.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(name='mapillary_tools',
-      version='0.1.0',
+      version='0.0.2',
       description='Mapillary Image/Video Import Pipeline',
       url='https://github.com/mapillary/mapillary_tools',
       author='Mapillary',
