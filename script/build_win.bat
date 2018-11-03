6826b887c25c61f77a73dd71fca6d01e3862bf2c
Santiago Baldassin <sbaldassin@gmail.com>
2018-11-03 10:35:32 -0300

    fix: moving the build artifacts to the publish dir

--- script/build_win.bat
+++ script/build_win.bat
@@ -1,3 +1,6 @@
 pip install -r requirements.txt
 pip install pyinstaller
-pyinstaller --distpath dist\\win --onefile --windowed mapillary_tools.spec 
+pyinstaller --distpath dist\win --onefile --windowed mapillary_tools.spec 
+
+mkdir -p publish
+mv dist\win publish\
