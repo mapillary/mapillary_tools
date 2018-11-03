44b36e27f268bf5d438d571a7663c6e0f0befe84
Santiago Baldassin <sbaldassin@gmail.com>
2018-11-03 09:48:53 -0300

    fix: building the binaries in different dirs to avoid clashing

--- script/build_win.bat
+++ script/build_win.bat
@@ -1,3 +1,3 @@
 pip install -r requirements.txt
 pip install pyinstaller
-pyinstaller --onefile --windowed mapillary_tools.spec
+pyinstaller --distpath dist\\win --onefile --windowed mapillary_tools.spec 
