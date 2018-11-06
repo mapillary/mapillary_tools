f1476d03725c18c9bbeada45fc7b221c7d405497
Santiago Baldassin <sbaldassin@gmail.com>
2018-11-05 23:12:38 -0300

    fix: fixing the copy command

--- script/build_win.bat
+++ script/build_win.bat
@@ -3,4 +3,4 @@ pip install pyinstaller
 pyinstaller --distpath dist\win --onefile --windowed mapillary_tools.spec 
 
 mkdir publisih\win
-xcopy dist\win\mapillary_tools.exe publish\win\mapillary_tools.exe
+xcopy dist\win\mapillary_tools.exe publish\win\
