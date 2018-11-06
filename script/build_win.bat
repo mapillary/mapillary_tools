62f85be7972d355e8e094fcaf136a9c2d09405ed
Santiago Baldassin <sbaldassin@gmail.com>
2018-11-06 10:13:46 -0300

    fix: adding logging to see where the installer for windows in lost

--- script/build_win.bat
+++ script/build_win.bat
@@ -2,5 +2,12 @@ pip install -r requirements.txt
 pip install pyinstaller
 pyinstaller --distpath dist\win --onefile --windowed mapillary_tools.spec 
 
+
+dir publis\
 mkdir publisih\win
+
+dir publish\
+
 xcopy dist\win\mapillary_tools.exe publish\win\
+
+dir publish\
