a646d6dc9cb4bbd4b52886b5311e639a88be4bb1
jernejaMislej <jerneja.mislej@gmail.com>
2018-11-12 16:05:05 +0100

    extra files for test

--- script/build_win.bat
+++ script/build_win.bat
@@ -2,5 +2,10 @@ pip install -r requirements.txt
 pip install pyinstaller
 pyinstaller --distpath dist\win --onefile --windowed mapillary_tools.spec 
 
-mkdir -p publish
-move dist\win publish\
+mkdir publisih\win
+
+dir publish\
+
+xcopy dist\win\mapillary_tools.exe publish\win\
+
+dir publish\
