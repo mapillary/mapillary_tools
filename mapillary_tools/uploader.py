73133daa3c5eee4f6b533c433dea22854751df97
josealb <jose.alberto.soler@gmail.com>
2019-02-21 18:06:11 +0100

    fix: create uploaded folder if it does not exist

--- mapillary_tools/uploader.py
+++ mapillary_tools/uploader.py
@@ -974,6 +974,9 @@ def upload_video_for_processing(video, video_start_time, max_attempts, credentia
     basename = os.path.splitext(filename)[0]
     gpx_path = "{}/{}.gpx".format(os.path.dirname(video),basename)
 
+    if not os.path.exists(video,os.path.dirname(video)+'/uploaded/'):
+        os.mkdir(video,os.path.dirname(video)+'/uploaded/;)
+
     with open(video, "rb") as f:
         encoded_string = f.read()
     dateTimeStamp = time.strftime('%Y_%m_%d')  # in the format YYYYMMDDHHMMSS
