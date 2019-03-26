7ec9db2c325b81afe67dfc53f86dc938b71f5115
josealb <jose.alberto.soler@gmail.com>
2019-03-26 17:28:13 +0100

    feat: move code to get upload url to a new function

--- mapillary_tools/uploader.py
+++ mapillary_tools/uploader.py
@@ -155,8 +155,21 @@ def flag_finalization(finalize_file_list):
         open(finalize_flag, 'a').close()
 
 
-def get_upload_url(user, password, upload_type):
-    return  # TODO ask mapillary_api 2.0 for url endpoint
+def get_upload_url(credentials):
+    '''
+    Returns upload URL using new upload API
+    '''
+    request_url = "https://a.mapillary.com/v3/users/{}/upload_secrets?client_id={}".format(
+        credentials["MAPSettingsUserKey"], CLIENT_ID)
+    request = urllib2.Request(request_url)
+    request.add_header('Authorization', 'Bearer {}'.format(
+        credentials["user_upload_token"]))
+    try:
+        response = json.loads(urllib2.urlopen(request).read())
+    except requests.exceptions.HTTPError as e:
+        print("Error getting upload parameters, upload could not start")
+        sys.exit(1)
+    return response
 
 
 def get_upload_file_list(import_path, skip_subfolders=False):
@@ -892,18 +905,7 @@ def send_videos_for_processing(video_import_path, user_name, user_email=None, us
         user_permission_hash = credentials["user_permission_hash"]
         user_signature_hash = credentials["user_signature_hash"]
 
-    request_url = "https://a.mapillary.com/v3/users/{}/upload_secrets?client_id={}".format(
-        credentials["MAPSettingsUserKey"], CLIENT_ID)
-    request = urllib2.Request(request_url)
-    request.add_header('Authorization', 'Bearer {}'.format(
-        credentials["user_upload_token"]))
-
-    try:
-        response = json.loads(urllib2.urlopen(request).read())
-    except requests.exceptions.HTTPError as e:
-        print("Error getting upload parameters, upload could not start")
-        sys.exit(1)
-
+    response = get_upload_url(credentials)
     request_params = response['videos']
 
     # upload all videos in the import path
