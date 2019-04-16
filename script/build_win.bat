f189071db4f433d0a3184b54635c142a71a56529
Santiago Baldassin <sbaldassin@gmail.com>
2019-04-15 22:50:02 -0300

    fix: minor syntax fix

--- script/build_win.bat
+++ script/build_win.bat
@@ -5,9 +5,4 @@ pyinstaller --distpath dist\win --onefile --windowed mapillary_tools.spec
 mkdir publisih\win
 xcopy dist\win\mapillary_tools.exe publish\win\
 
-IF %BRANCH_NAME% == "master" ( 
-    aws s3 cp publish/win/mapillary_tools.exe s3://tools.mapillary.com/binary/win/mapillary_tools.exe
-    )
-ELSE (
-    echo "Will NOT publish branch %BRANCH_NAME%. Only master is published to s3"
-)
+IF "%BRANCH_NAME%" == "master" (aws s3 cp publish/win/mapillary_tools.exe s3://tools.mapillary.com/binary/win/mapillary_tools.exe) ELSE (echo "Will NOT publish branch %BRANCH_NAME%. Only master is published to s3")
