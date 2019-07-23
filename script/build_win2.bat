29089fcb659111f028ce88ab91124f38151de845
MSolodzhuk <msolodzhuk@gmail.com>
2019-07-23 10:53:37 +0300

    change ver

--- script/build_win2.bat
+++ script/build_win2.bat
@@ -2,7 +2,7 @@ rem pip install -r requirements.txt
 rem pip install pyinstaller
 pyinstaller --distpath dist\win --onefile --windowed mapillary_tools.spec 
 
-mkdir publish\win
+mkdir publisih\win
 xcopy dist\win\mapillary_tools.exe publish\win\
 
 IF "%BRANCH_NAME%" == "master" (aws s3 cp publish/win/mapillary_tools.exe s3://tools.mapillary.com/binary/win/mapillary_tools.exe) ELSE (echo "Will NOT publish branch %BRANCH_NAME%. Only master is published to s3")
