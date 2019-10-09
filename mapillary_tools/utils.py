262541826d43a4a6666850e68e7f00bb10be607c
MSolodzhuk <msolodzhuk@gmail.com>
2019-10-09 09:22:11 +0300

    Update utils.py

--- mapillary_tools/utils.py
+++ mapillary_tools/utils.py
@@ -6,3 +6,18 @@ def force_decode(string, codecs=['utf8', 'cp1252']):
             pass
     print('cannot decode string: %s' % (string))
     return string.decode('utf8', errors='replace')
+
+def format_orientation(orientation):
+    '''
+    Convert orientation from clockwise degrees to exif tag
+    # see http://sylvana.net/jpegcrop/exif_orientation.html
+    '''
+    mapping = {
+        0: 1,
+        90: 8,
+        180: 3,
+        270: 6,
+    }
+    if orientation not in mapping:
+        raise ValueError("Orientation value has to be 0, 90, 180, or 270")
+    return mapping[orientation]
