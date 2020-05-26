4d971cf10f8a907cc3c224afe99ada4d1246148f
Matias Volpe <matias.volpe@gmail.com>
2020-05-26 15:25:04 -0300

    feat: add query string params to upload_api

--- mapillary_tools/upload_api.py
+++ mapillary_tools/upload_api.py
@@ -4,17 +4,19 @@ import requests
 def create_upload_session(upload_type, metadata, options):
     url = _get_url(options, "/v3/me/uploads")
     headers = _get_headers(options)
+    params = _get_params(options)
     json = {"type": upload_type, "metadata": metadata}
 
-    return requests.post(url, headers=headers, json=json)
+    return requests.post(url, params=params, headers=headers, json=json)
 
 
 def close_upload_session(session, json, options):
     url = _get_url(options, "/v3/me/uploads")
     url = "{}/{}/closed".format(url, session["key"])
     headers = _get_headers(options)
+    params = _get_params(options)
 
-    return requests.put(url, headers=headers, json=json)
+    return requests.put(url, params=params, headers=headers, json=json)
 
 
 def upload_file(session, file_path, object_key):
@@ -39,3 +41,10 @@ def _get_headers(options):
     headers = {"Authorization": "Bearer " + token}
 
     return headers
+
+
+def _get_params(options):
+    client_id = options["client_id"]
+    params = {"client_id": client_id}
+
+    return params
