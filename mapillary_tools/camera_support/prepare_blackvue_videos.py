88d93874b40df9e2d9cd45532885f50dcb36e268
Tao Peng <ptpttt@gmail.com>
2021-05-18 20:46:52 +0200

    fix: convert devide model to unicode

--- mapillary_tools/camera_support/prepare_blackvue_videos.py
+++ mapillary_tools/camera_support/prepare_blackvue_videos.py
@@ -2,7 +2,7 @@ from pymp4.parser import Box
 import io
 
 
-def find_camera_model(videos_folder):
+def find_camera_model(videos_folder) -> bytes:
     from mapillary_tools.uploader import get_video_file_list
 
     file_list = get_video_file_list(videos_folder)
@@ -18,10 +18,12 @@ def find_camera_model(videos_folder):
         box = Box.parse_stream(fd)
         if box.type.decode("utf-8") == "free":  # or 'ftyp':
             return box.data[29:39]
+    raise RuntimeError(f"camera model not found in {file_list[0]}")
 
 
 def apply_config_blackvue(vars_args):
-    vars_args["device_model"] = find_camera_model(vars_args["video_import_path"])
+    model = find_camera_model(vars_args["video_import_path"])
+    vars_args["device_model"] = model.decode("utf-8")
     vars_args["device_make"] = "Blackvue"
     vars_args["geotag_source"] = "blackvue_videos"
     vars_args["duplicate_angle"] = 360
