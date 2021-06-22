934a35b000ae158e0d35eb7456d129db2b94bc15
Tao Peng <ptpttt@gmail.com>
2021-06-22 20:35:38 +0200

    simplify build script

--- script/build_win.bat
+++ script/build_win.bat
@@ -1,6 +1,1 @@
 pyinstaller --noconfirm --distpath dist\win --onefile --windowed mapillary_tools.spec
-
-mkdir publish\win
-xcopy dist\win\mapillary_tools.exe publish\win\
-
-IF "%BRANCH_NAME%" == "master" (aws s3 cp publish/win/mapillary_tools.exe s3://tools.mapillary.com/binary/win/mapillary_tools.exe) ELSE (echo "Will NOT publish branch %BRANCH_NAME%. Only master is published to s3")
