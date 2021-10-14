83130368a2ef59d2cba93c87e6f1ffbef535d55c
Tao Peng <ptpttt@gmail.com>
2021-10-14 16:56:21 +0200

    Fix: add the missing mapillary.geotag module and format setup.py

--- setup.py
+++ setup.py
@@ -6,26 +6,27 @@ here = os.path.abspath(os.path.dirname(__file__))
 
 
 def read_requirements():
-    with open('requirements.txt') as fp:
+    with open("requirements.txt") as fp:
         return [row.strip() for row in fp if row.strip()]
 
 
 about = {}
-with open(os.path.join(here, 'mapillary_tools', '__init__.py'), 'r') as f:
+with open(os.path.join(here, "mapillary_tools", "__init__.py"), "r") as f:
     exec(f.read(), about)
 
 
-setup(name='mapillary_tools',
-      version=about['VERSION'],
-      description='Mapillary Image/Video Import Pipeline',
-      url='https://github.com/mapillary/mapillary_tools',
-      author='Mapillary',
-      license='BSD',
-      python_requires='>=3.6',
-      packages=['mapillary_tools', 'mapillary_tools.commands'],
-      entry_points='''
+setup(
+    name="mapillary_tools",
+    version=about["VERSION"],
+    description="Mapillary Image/Video Import Pipeline",
+    url="https://github.com/mapillary/mapillary_tools",
+    author="Mapillary",
+    license="BSD",
+    python_requires=">=3.6",
+    packages=["mapillary_tools", "mapillary_tools.commands", "mapillary_tools.geotag"],
+    entry_points="""
       [console_scripts]
       mapillary_tools=mapillary_tools.__main__:main
-      ''',
-      install_requires=read_requirements(),
+      """,
+    install_requires=read_requirements(),
 )
