65d3115845549320c0e84977d7505efb27a9eb4a
Tao Peng <ptpttt@gmail.com>
2022-06-27 14:19:54 -0700

    Add a simple mp4 parser to replace pymp4 (#506)
    * add simple_mp4_parser.py
    
    * fix pyinstaller
    
    * parse blackvue camera model
    
    * quit the searching if model not found
    
    * move the pymp4-based parser to blackvue_utils_pymp4

---
+++ mapillary_tools/geotag/blackvue_utils.py
@@ -0,0 +1,82 @@
+import typing as T
+
+from .. import types
+from .simple_mp4_parser import parse_boxes
+from .blackvue_utils_pymp4 import _parse_gps_box
+from . import utils as geotag_utils
+
+
+def find_camera_model(path: str) -> str:
+    with open(path, "rb") as fp:
+        for header, stream in parse_boxes(fp):
+            if header.type == b"free":
+                return parse_camera_model_from_free_box(stream, maxsize=header.maxsize)
+    return ""
+
+
+def parse_camera_model_from_free_box(stream: T.BinaryIO, maxsize: int) -> str:
+    for h, s in parse_boxes(stream, maxsize=maxsize):
+        if h.type == b"cprt":
+            cprt = s.read(h.maxsize)
+            # An example cprt: b' Pittasoft Co., Ltd.;DR900S-1CH;'
+            fields = cprt.split(b";")
+            if 2 <= len(fields):
+                model: bytes = fields[1]
+                if model:
+                    try:
+                        return model.decode("utf8")
+                    except UnicodeDecodeError:
+                        return ""
+            else:
+                return ""
+    return ""
+
+
+def parse_gps_from_free_box(
+    stream: T.BinaryIO, maxsize: int
+) -> T.Optional[T.List[types.GPXPoint]]:
+    points = None
+    for h, s in parse_boxes(stream, maxsize=maxsize):
+        if h.type == b"gps ":
+            gps_data = s.read(h.maxsize)
+            if points is None:
+                points = []
+            points.extend(_parse_gps_box(gps_data, False))
+    return points
+
+
+def parse_gps_points(path: str) -> T.List[types.GPXPoint]:
+    points = None
+
+    with open(path, "rb") as fp:
+        for header, stream in parse_boxes(fp):
+            if header.type == b"free":
+                points = parse_gps_from_free_box(stream, maxsize=header.maxsize)
+                if points is not None:
+                    break
+
+    if points is None:
+        points = []
+
+    points.sort()
+
+    return [types.GPXPoint(time=p[0], lat=p[1], lon=p[2], alt=p[3]) for p in points]
+
+
+if __name__ == "__main__":
+    import sys, os
+    from .. import utils
+
+    def _concert(path: str):
+        points = parse_gps_points(path)
+        gpx = geotag_utils.convert_points_to_gpx(points)
+        model = find_camera_model(path)
+        gpx.description = f"Extracted from {model}"
+        print(gpx.to_xml())
+
+    for path in sys.argv[1:]:
+        if os.path.isdir(path):
+            for p in utils.get_video_file_list(path, abs_path=True):
+                _concert(p)
+        else:
+            _concert(path)
---
+++ mapillary_tools/geotag/simple_mp4_parser.py
@@ -0,0 +1,161 @@
+import io
+import typing as T
+
+
+class Header(T.NamedTuple):
+    # non-negative: 0 indicates no more boxes
+    header_size: int
+    type: bytes
+    # positive: box size includes header; 0 indicates open ended
+    box_size: int
+    # either -1 or positive: data size that can be passed to the read function; -1 indicates open ended
+    maxsize: int
+
+
+class RangeError(Exception):
+    pass
+
+
+def _size_remain(size: int, bound: int) -> int:
+    assert 0 <= size and (bound == -1 or 0 <= bound), f"got size={size} bound={bound}"
+
+    if bound == -1:
+        return -1
+
+    remaining = bound - size
+    if remaining < 0:
+        raise RangeError(f"request to read {size} bytes but {bound} bytes remain")
+    return remaining
+
+
+def parse_box_header(stream: T.BinaryIO, maxsize: int = -1) -> Header:
+    assert maxsize == -1 or 0 <= maxsize
+
+    def _read(size: int) -> bytes:
+        nonlocal maxsize
+        size = size if maxsize == -1 else min(size, maxsize)
+        assert 0 <= size
+        data = stream.read(size)
+        maxsize = _size_remain(len(data), maxsize)
+        return data
+
+    offset_start = stream.tell()
+
+    # box size
+    box_size = int.from_bytes(_read(4), "big", signed=False)
+
+    # type
+    box_type = _read(4)
+
+    # large box size that follows box type
+    if box_size == 1:
+        box_size = int.from_bytes(_read(8), "big", signed=False)
+
+    # header size
+    offset_end = stream.tell()
+    assert offset_start <= offset_end
+    header_size = offset_end - offset_start
+
+    # maxsize
+    if box_size != 0:
+        data_size = _size_remain(header_size, box_size)
+        _size_remain(data_size, maxsize)
+        maxsize = data_size
+
+    return Header(
+        header_size=header_size,
+        type=box_type,
+        box_size=box_size,
+        maxsize=maxsize,
+    )
+
+
+def parse_boxes(
+    stream: T.BinaryIO,
+    maxsize: int = -1,
+) -> T.Generator[T.Tuple[Header, T.BinaryIO], None, None]:
+    assert maxsize == -1 or 0 <= maxsize
+
+    while True:
+        offset = stream.tell()
+        header = parse_box_header(stream, maxsize=maxsize)
+
+        if not header.header_size:
+            break
+
+        yield header, stream
+
+        # ajust offset and maxsize for the next box parsing
+        if header.box_size == 0:
+            if maxsize == -1:
+                stream.seek(0, io.SEEK_END)
+            else:
+                stream.seek(offset + maxsize, io.SEEK_SET)
+            maxsize = 0
+        else:
+            stream.seek(offset + header.box_size, io.SEEK_SET)
+            maxsize = _size_remain(header.box_size, maxsize)
+
+
+def parse_boxes_recursive(
+    stream: T.BinaryIO,
+    maxsize: int = -1,
+    depth: int = 0,
+    recursive_types: T.Optional[T.Set[bytes]] = None,
+) -> T.Generator[T.Tuple[Header, int, T.BinaryIO], None, None]:
+    assert maxsize == -1 or 0 <= maxsize
+
+    if recursive_types is None:
+        recursive_types = set()
+
+    for header, box in parse_boxes(stream, maxsize=maxsize):
+        offset = box.tell()
+        yield header, depth, stream
+        if header.type in recursive_types:
+            box.seek(offset, io.SEEK_SET)
+            yield from parse_boxes_recursive(
+                stream,
+                maxsize=header.maxsize,
+                depth=depth + 1,
+                recursive_types=recursive_types,
+            )
+
+
+if __name__ == "__main__":
+    import sys, os
+    from .. import utils
+
+    recursive_types = {
+        b"moov",
+        b"moof",
+        b"traf",
+        b"mvex",
+        b"trak",
+        b"mdia",
+        b"minf",
+        b"dinf",
+        b"stbl",
+        b"schi",
+        b"gmhd",
+    }
+
+    def _parse_file(path: str):
+        with open(path, "rb") as fp:
+            for h, d, s in parse_boxes_recursive(fp, recursive_types=recursive_types):
+                margin = "\t" * d
+                try:
+                    utfh = h.type.decode("utf8")
+                except UnicodeDecodeError:
+                    utfh = str(h)
+                header = f"{utfh} {h.box_size}:"
+                if h.type in recursive_types:
+                    print(margin, header)
+                else:
+                    print(margin, header, s.read(h.maxsize)[:32])
+
+    for path in sys.argv[1:]:
+        if os.path.isdir(path):
+            for p in utils.get_video_file_list(path, abs_path=True):
+                _parse_file(p)
+        else:
+            _parse_file(path)
