033dfbe6ddcb0f3c5885e9959778767e6492b190
Tao Peng <ptpttt@gmail.com>
2022-07-18 10:46:50 -0700

    add initial camm parser

---
+++ mapillary_tools/geotag/camm_parser.py
@@ -0,0 +1,123 @@
+import datetime
+import io
+import typing as T
+
+import construct as C
+
+from .simple_mp4_parser import Sample, parse_path, parse_samples_from_trak
+
+Float = C.Float32l
+Double = C.Float64l
+
+
+CAMMSampleData = C.Struct(
+    C.Padding(2),
+    "type" / C.Int16ul,
+    "data"
+    / C.Switch(
+        C.this.type,
+        {
+            # angle_axis
+            0: Float[3],
+            1: C.Struct(
+                "pixel_exposure_time" / C.Int32sl,
+                "rolling_shutter_skew_time" / C.Int32sl,
+            ),
+            # gyro
+            2: Float[3],
+            # acceleration
+            3: Float[3],
+            # position
+            4: Float[3],
+            # lat, lon, alt
+            5: Double[3],
+            6: C.Struct(
+                "time_gps_epoch" / Double,
+                "gps_fix_type" / C.Int32sl,
+                "latitude" / Double,
+                "longitude" / Double,
+                "altitude" / Float,
+                "horizontal_accuracy" / Float,
+                "vertical_accuracy" / Float,
+                "velocity_east" / Float,
+                "velocity_north" / Float,
+                "velocity_up" / Float,
+                "speed_accuracy" / Float,
+            ),
+            # magnetic_field
+            7: Float[3],
+        },
+    ),
+)
+
+
+class DeltaPoint(T.NamedTuple):
+    # seconds since the beginning of the stream presentation
+    delta: float
+    lat: float
+    lon: float
+    alt: float
+
+
+def _extract_delta_points(fp: T.BinaryIO, samples: T.Iterable[Sample]):
+    for sample in samples:
+        fp.seek(sample.offset, io.SEEK_SET)
+        data = fp.read(sample.size)
+        box = CAMMSampleData.parse(data)
+        if box.type == 5:
+            yield DeltaPoint(
+                delta=sample.delta,
+                lat=box.data[0],
+                lon=box.data[1],
+                alt=box.data[2],
+            )
+        elif box.type == 6:
+            # Not using box.data.time_gps_epoch as the point timestamp
+            # because it is from another clock
+            yield DeltaPoint(
+                delta=sample.delta,
+                lat=box.data.latitude,
+                lon=box.data.longitude,
+                alt=box.data.altitude,
+            )
+
+
+def parse_gpx(path: str) -> T.List[DeltaPoint]:
+    with open(path, "rb") as fp:
+        for h, s in parse_path(fp, [b"moov", b"trak"]):
+            camm_samples = (
+                sample
+                for sample in parse_samples_from_trak(s, maxsize=h.maxsize)
+                if sample.description.format == b"camm"
+            )
+            points = list(_extract_delta_points(fp, camm_samples))
+            if points:
+                return points
+    return []
+
+
+if __name__ == "__main__":
+    import sys, os
+    from .. import utils, types
+    from . import utils as geotag_utils
+
+    def _convert(path: str):
+        delta_points = parse_gpx(path)
+        points = [
+            types.GPXPoint(
+                time=datetime.datetime.utcfromtimestamp(p.delta),
+                lat=p.lat,
+                lon=p.lon,
+                alt=p.alt,
+            )
+            for p in delta_points
+        ]
+        gpx = geotag_utils.convert_points_to_gpx(points)
+        print(gpx.to_xml())
+
+    for path in sys.argv[1:]:
+        if os.path.isdir(path):
+            for p in utils.get_video_file_list(path, abs_path=True):
+                _convert(p)
+        else:
+            _convert(path)
---
+++ mapillary_tools/geotag/geotag_from_camm.py
@@ -0,0 +1,94 @@
+import datetime
+import os
+import typing as T
+import logging
+
+from tqdm import tqdm
+
+from .. import types, utils, exceptions
+from ..geo import get_max_distance_from_start
+from . import camm_parser, utils as geotag_utils
+from .geotag_from_generic import GeotagFromGeneric
+from .geotag_from_gpx import GeotagFromGPXWithProgress
+
+
+LOG = logging.getLogger(__name__)
+
+
+class GeotagFromCAMM(GeotagFromGeneric):
+    def __init__(
+        self,
+        image_dir: str,
+        source_path: str,
+        offset_time: float = 0.0,
+    ):
+        self.image_dir = image_dir
+        if os.path.isdir(source_path):
+            self.videos = utils.get_video_file_list(source_path, abs_path=True)
+        else:
+            # it is okay to not suffix with .mp4
+            self.videos = [source_path]
+        self.offset_time = offset_time
+        super().__init__()
+
+    def to_description(self) -> T.List[types.ImageDescriptionFileOrError]:
+        descs: T.List[types.ImageDescriptionFileOrError] = []
+        images = utils.get_image_file_list(self.image_dir)
+        for video in self.videos:
+            LOG.debug("Processing CAMM video: %s", video)
+
+            sample_images = utils.filter_video_samples(images, video)
+            LOG.debug(
+                "Found %d sample images from video %s",
+                len(sample_images),
+                video,
+            )
+
+            if not sample_images:
+                continue
+
+            points = camm_parser.parse_gpx(video)
+
+            # bypass empty points to raise MapillaryGPXEmptyError
+            if points and geotag_utils.is_video_stationary(
+                get_max_distance_from_start([(p.lat, p.lon) for p in points])
+            ):
+                LOG.warning(
+                    "Fail %d sample images due to stationary video %s",
+                    len(sample_images),
+                    video,
+                )
+                for image in sample_images:
+                    err = types.describe_error(
+                        exceptions.MapillaryStationaryVideoError(
+                            "Stationary CAMM video"
+                        )
+                    )
+                    descs.append({"error": err, "filename": image})
+                continue
+
+            with tqdm(
+                total=len(sample_images),
+                desc=f"Interpolating {os.path.basename(video)}",
+                unit="images",
+                disable=LOG.getEffectiveLevel() <= logging.DEBUG,
+            ) as pbar:
+                geotag = GeotagFromGPXWithProgress(
+                    self.image_dir,
+                    sample_images,
+                    [
+                        types.GPXPoint(
+                            time=datetime.datetime.utcfromtimestamp(p.delta),
+                            lat=p.lat,
+                            lon=p.lon,
+                            alt=p.alt,
+                        )
+                        for p in points
+                    ],
+                    use_gpx_start_time=False,
+                    use_image_start_time=True,
+                    offset_time=self.offset_time,
+                    progress_bar=pbar,
+                )
+                descs.extend(geotag.to_description())
+        return descs
