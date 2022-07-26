61b9e36327edf0e863d46fa4763d381c85c5111f
Tao Peng <ptpttt@gmail.com>
2022-07-26 12:44:08 -0700

    pip install wheel

--- script/build_win.ps1
+++ script/build_win.ps1
@@ -7,7 +7,10 @@ python3 -m pip uninstall -y pyinstaller
 git clone --depth=1 --branch v5.2 https://github.com/pyinstaller/pyinstaller.git pyinstaller_git
 cd pyinstaller_git/bootloader
 python3 ./waf all
+git diff
 cd ..
+# Error: Building wheels requires the 'wheel' package. Please `pip install wheel` then try again.
+python3 -m pip install wheel
 python3 setup.py install
 pyinstaller --version
 pyinstaller --noconfirm --distpath dist\win mapillary_tools.spec
