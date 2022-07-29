778e66db8eb4fdd4cb3bece8d6a7f7c370844a67
Tao Peng <ptpttt@gmail.com>
2022-07-29 10:26:15 -0700

    fix: build bootloader for windows in the CI (#516)
    * fix: build bootloader for windows in the CI
    
    * clone into a new folder
    
    * fix Host key verification failed
    
    * pip install wheel
    
    * fix pwd
    
    * reset workflow
    
    * add some comments
    
    * fix the building issue for python3.6
    
    * add build_bootloader.ps1
    
    * pyinstaller build for python3.8 only
    
    * set up architechture matrix
    
    * fix arch setup
    
    * fix
    
    * fix
    
    * fix
    
    * add comment
    
    * fix arch

--- script/build_win.ps1
+++ script/build_win.ps1
@@ -1,7 +1,17 @@
 $OS="win"
+# this is OS arch
+# $ARCH=(wmic OS get OSArchitecture)[2]
+$MAXSIZE32=python3 -c "import sys; print(sys.maxsize <= 2**32)"
+# -ceq case-sensitive equality https://docs.microsoft.com/en-us/powershell/scripting/learn/deep-dives/everything-about-if
+if ($MAXSIZE32 -ceq "True") {
+    $ARCH="32bit"
+} else {
+    $ARCH="64bit"
+}
 
 # build
 mkdir -Force dist
+pyinstaller --version
 pyinstaller --noconfirm --distpath dist\win mapillary_tools.spec
 
 # check
@@ -9,14 +19,14 @@ $SOURCE="dist\win\mapillary_tools.exe"
 dist\win\mapillary_tools.exe --version
 $VERSION_OUTPUT=dist\win\mapillary_tools.exe --version
 $VERSION=$VERSION_OUTPUT.split(' ')[2]
-$TARGET="dist\releases\mapillary_tools-$VERSION-$OS.exe"
+$TARGET="dist\releases\mapillary_tools-$VERSION-$OS-$ARCH.exe"
 
 # package
 mkdir -Force dist\releases
 Copy-Item "$SOURCE" "$TARGET"
 
 # sha256
-Get-FileHash $TARGET -Algorithm SHA256 | Select-Object Hash > "$TARGET.sha256"
+Get-FileHash $TARGET -Algorithm SHA256 | Select-Object Hash > "$TARGET.sha256.txt"
 
 # summary
 Get-ChildItem dist\releases
