d3a24fefae7a6c18a343c1bd8792869d331dce08
Tao Peng <ptpttt@gmail.com>
2022-08-15 12:18:14 -0700

    handle sample chunks

--- mapillary_tools/geotag/simple_mp4_builder.py
+++ mapillary_tools/geotag/simple_mp4_builder.py
@@ -22,6 +22,10 @@ Box: C.Struct
 
 LazyBox = C.LazyBound(lambda: C.GreedyRange(Box))
 
+# The struct is for building only.
+# For parsing we should use the utils provided in simple_mp4_parser.
+# Because it does not handle the extended size,
+# although normally a moov struct won't have an extended size.
 Box = C.Prefixed(
     C.Int32ub,
     C.Struct(
@@ -90,67 +94,76 @@ def _build_stsz(sizes: T.Sequence[int]) -> BoxDict:
 
 
 @dataclasses.dataclass
-class _CompressedSampleChunk:
-    __slots__ = ("samples_per_chunk", "sample_description_index")
+class _SampleChunk:
+    __slots__ = ("first_chunk", "samples_per_chunk", "sample_description_index")
+    # make sure dataclasses.asdict() produce the result as SampleToChunkBox expects
+    first_chunk: int
     samples_per_chunk: int
     sample_description_index: int
 
 
-def _build_stsc(
-    descriptions: T.Sequence[T.Any], raw_samples: T.Iterable[RawSample]
-) -> BoxDict:
-    # compress samples by description
-    compressed: T.List[_CompressedSampleChunk] = []
-    for raw_sample in raw_samples:
-        assert (
-            1 <= raw_sample.description_idx <= len(descriptions)
-        ), f"invalid sample description index {raw_sample.description_idx}: must be 1-based and <= {len(descriptions)}"
-        if (
-            compressed
-            and raw_sample.description_idx == compressed[-1].sample_description_index
-        ):
-            compressed[-1].samples_per_chunk += 1
-        else:
-            compressed.append(_CompressedSampleChunk(1, raw_sample.description_idx))
+def _build_chunks(raw_samples: T.Iterable[RawSample]) -> T.List[_SampleChunk]:
+    chunks: T.List[_SampleChunk] = []
+    prev_raw_sample = None
 
+    for raw_sample in raw_samples:
+        if (
+            # if it has the sampe description index as the previous sample
+            (
+                chunks
+                and raw_sample.description_idx == chunks[-1].sample_description_index
+            )
+            # if it is next to the previous sample (contiguous)
+            and (
+                prev_raw_sample
+                and raw_sample.offset == prev_raw_sample.offset + prev_raw_sample.size
+            )
+        ):
+            # add this sample to the current chunk
+            chunks[-1].samples_per_chunk += 1
+        else:
+            # 1-based first_chunk
+            first_chunk = len(chunks) + 1
+            chunks.append(_SampleChunk(first_chunk, 1, raw_sample.description_idx))
+
+        prev_raw_sample = raw_sample
+
+    # TODO: compress chunks here
+
+    return chunks
+
+
+def _build_stsc(raw_samples: T.Iterable[RawSample]) -> BoxDict:
+    chunks = _build_chunks(raw_samples)
     return {
         "type": b"stsc",
         "data": {
-            "entries": [
-                {
-                    "first_chunk": idx + 1,
-                    "samples_per_chunk": entry.samples_per_chunk,
-                    "sample_description_index": entry.sample_description_index,
-                }
-                for idx, entry in enumerate(compressed)
-            ],
+            "entries": [dataclasses.asdict(chunk) for chunk in chunks],
         },
     }
 
 
 @dataclasses.dataclass
 class _CompressedSampleDelta:
-    __slots__ = ("count", "delta")
-    count: int
-    delta: int
+    __slots__ = ("sample_count", "sample_delta")
+    # make sure dataclasses.asdict() produce the result as SampleSizeBox expects
+    sample_count: int
+    sample_delta: int
 
 
 def _build_stts(sample_deltas: T.Iterable[int]) -> BoxDict:
     # compress deltas
     compressed: T.List[_CompressedSampleDelta] = []
     for delta in sample_deltas:
-        if compressed and delta == compressed[-1].delta:
-            compressed[-1].count += 1
+        if compressed and delta == compressed[-1].sample_delta:
+            compressed[-1].sample_count += 1
         else:
             compressed.append(_CompressedSampleDelta(1, delta))
 
     return {
         "type": b"stts",
         "data": {
-            "entries": [
-                {"sample_count": td.count, "sample_delta": td.delta}
-                for td in compressed
-            ],
+            "entries": [dataclasses.asdict(td) for td in compressed],
         },
     }
 
@@ -177,6 +190,6 @@ def build_stbl_from_raw_samples(
             _build_stsz([s.size for s in raw_samples]),
             _build_stco_or_co64([s.offset for s in raw_samples]),
             _build_stts((s.timedelta for s in raw_samples)),
-            _build_stsc(descriptions, raw_samples),
+            _build_stsc(raw_samples),
         ],
     }
