3ee85db9a6e6b56f4856807ec5c0a06e095cb010
Tao Peng <ptpttt@gmail.com>
2022-08-29 15:00:21 -0700

    separate GPX tracks

--- tests/cli/blackvue_parser.py
+++ tests/cli/blackvue_parser.py
@@ -2,25 +2,32 @@ import os
 import pathlib
 import sys
 
+import gpxpy
+
 from mapillary_tools import utils
 from mapillary_tools.geotag import blackvue_parser, utils as geotag_utils
 
 
-def _convert(path: pathlib.Path):
+def _convert_to_track(path: pathlib.Path):
+    track = gpxpy.gpx.GPXTrack()
     points = blackvue_parser.parse_gps_points(path)
-    gpx = geotag_utils.convert_points_to_gpx(points)
+    segment = geotag_utils.convert_points_to_gpx_segment(points)
+    track.segments.append(segment)
     model = blackvue_parser.find_camera_model(path)
-    gpx.description = f"Extracted from {model}"
-    print(gpx.to_xml())
+    track.description = f"Extracted from {model}"
+    track.name = path.name
+    return track
 
 
 def main():
+    gpx = gpxpy.gpx.GPX()
     for path in sys.argv[1:]:
         if os.path.isdir(path):
             for p in utils.get_video_file_list(path, abs_path=True):
-                _convert(pathlib.Path(p))
+                gpx.tracks.append(_convert_to_track(pathlib.Path(p)))
         else:
-            _convert(pathlib.Path(path))
+            gpx.tracks.append(_convert_to_track(pathlib.Path(path)))
+    print(gpx.to_xml())
 
 
 if __name__ == "__main__":
