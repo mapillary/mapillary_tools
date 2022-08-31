9b2df7611ee061caaf3a2f86eebf89df3c9dda85
Tao Peng <ptpttt@gmail.com>
2022-08-30 17:05:01 -0700

    add tests

---
+++ tests/unit/test_exceptions.py
@@ -0,0 +1,27 @@
+import json
+
+from mapillary_tools import exceptions, types
+
+
+def test_all():
+    all_excs = [
+        getattr(exceptions, ex)
+        for ex in dir(exceptions)
+        if ex.startswith("Mapillary") and ex.endswith("Error")
+    ]
+
+    all_desc_excs = [
+        exc
+        for exc in all_excs
+        if issubclass(exc, exceptions._MapillaryDescriptionError)
+    ]
+
+    for exc in all_desc_excs:
+        if exc is exceptions.MapillaryOutsideGPXTrackError:
+            e = exc("hello", "world", "hey", "aa")
+        elif exc is exceptions.MapillaryDuplicationError:
+            e = exc("hello", {})
+        else:
+            e = exc("hello")
+        # should not raise
+        json.dumps(types.describe_error(e, "test"))
