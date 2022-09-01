05f19db159b69afdf7f345b43c433848cb9c7a81
Tao Peng <ptpttt@gmail.com>
2022-09-01 10:04:50 -0700

    missed the file

---
+++ mapillary_tools/commands/upload_camm.py
@@ -0,0 +1,32 @@
+import inspect
+from pathlib import Path
+
+from .. import constants
+from ..upload import upload_multiple
+from .upload import Command as UploadCommand
+
+
+class Command:
+    name = "upload_camm"
+    help = "upload CAMM videos to Mapillary"
+
+    def add_basic_arguments(self, parser):
+        parser.add_argument(
+            "import_path",
+            help="Path to your CAMM videos.",
+            nargs="+",
+            type=Path,
+        )
+        group = parser.add_argument_group(
+            f"{constants.ANSI_BOLD}UPLOAD OPTIONS{constants.ANSI_RESET_ALL}"
+        )
+        UploadCommand.add_common_upload_options(group)
+
+    def run(self, vars_args: dict):
+        args = {
+            k: v
+            for k, v in vars_args.items()
+            if k in inspect.getfullargspec(upload_multiple).args
+        }
+        args["file_type"] = "camm"
+        upload_multiple(**args)
