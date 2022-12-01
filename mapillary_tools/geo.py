2a32f4cfeda95aaa7612a097e504ebed6f15b577
Tao Peng <ptpttt@gmail.com>
2022-11-30 23:13:40 -0800

    add Interpolator

--- mapillary_tools/geo.py
+++ mapillary_tools/geo.py
@@ -11,6 +11,24 @@ WGS84_a = 6378137.0
 WGS84_b = 6356752.314245
 
 
+@dataclasses.dataclass(order=True)
+class Point:
+    # For reducing object sizes
+    # dataclass(slots=True) not available until 3.10
+    __slots__ = (
+        "time",
+        "lat",
+        "lon",
+        "alt",
+        "angle",
+    )
+    time: float
+    lat: float
+    lon: float
+    alt: T.Optional[float]
+    angle: T.Optional[float]
+
+
 def ecef_from_lla(lat: float, lon: float, alt: float) -> T.Tuple[float, float, float]:
     """
     Compute ECEF XYZ from latitude, longitude and altitude.
@@ -113,22 +131,18 @@ def pairwise(iterable: T.Iterable[_IT]) -> T.Iterable[T.Tuple[_IT, _IT]]:
     return zip(a, b)
 
 
-@dataclasses.dataclass(order=True)
-class Point:
-    # For reducing object sizes
-    # dataclass(slots=True) not available until 3.10
-    __slots__ = (
-        "time",
-        "lat",
-        "lon",
-        "alt",
-        "angle",
-    )
-    time: float
-    lat: float
-    lon: float
-    alt: T.Optional[float]
-    angle: T.Optional[float]
+def group_every(
+    iterable: T.Iterable[_IT], n: int
+) -> T.Generator[T.Generator[_IT, None, None], None, None]:
+    """
+    Return a generator that divides the iterable into groups by N.
+    """
+
+    if not (0 < n):
+        raise ValueError("expect 0 < n but got {0}".format(n))
+
+    for _, group in itertools.groupby(enumerate(iterable), key=lambda t: t[0] // n):
+        yield (item for _, item in group)
 
 
 def as_unix_time(dt: T.Union[datetime.datetime, int, float]) -> float:
@@ -142,7 +156,46 @@ def as_unix_time(dt: T.Union[datetime.datetime, int, float]) -> float:
     return aware_dt.timestamp()
 
 
-def interpolate(points: T.Sequence[Point], t: float) -> Point:
+def _interpolate_segment(start: Point, end: Point, t: float) -> Point:
+    if start.time == end.time:
+        weight = 0.0
+    else:
+        weight = (t - start.time) / (end.time - start.time)
+
+    lat = start.lat + (end.lat - start.lat) * weight
+    lon = start.lon + (end.lon - start.lon) * weight
+    angle = compute_bearing(start.lat, start.lon, end.lat, end.lon)
+    alt: T.Optional[float]
+    if start.alt is not None and end.alt is not None:
+        alt = start.alt + (end.alt - start.alt) * weight
+    else:
+        alt = None
+
+    return Point(time=t, lat=lat, lon=lon, alt=alt, angle=angle)
+
+
+def _interpolate_at_index(points: T.Sequence[Point], t: float, idx: int):
+    assert points, "expect non-empty points"
+
+    # find the segment (start point, end point)
+    if len(points) == 1:
+        start, end = points[0], points[0]
+    else:
+        if 0 < idx < len(points):
+            # interpolating within the range
+            start, end = points[idx - 1], points[idx]
+        elif idx <= 0:
+            # extrapolating behind the range
+            start, end = points[0], points[1]
+        else:
+            # extrapolating beyond the range
+            assert len(points) <= idx
+            start, end = points[-2], points[-1]
+
+    return _interpolate_segment(start, end, t)
+
+
+def interpolate(points: T.Sequence[Point], t: float, lo: int = 0) -> Point:
     """
     Interpolate or extrapolate the point at time t along the sequence of points (sorted by time).
     """
@@ -154,36 +207,85 @@ def interpolate(points: T.Sequence[Point], t: float) -> Point:
     #     assert cur.time <= nex.time, "Points not sorted"
 
     p = Point(time=t, lat=float("-inf"), lon=float("-inf"), alt=None, angle=None)
-    idx = bisect.bisect_left(points, p)
+    idx = bisect.bisect_left(points, p, lo=lo)
+    return _interpolate_at_index(points, t, idx)
 
-    if 0 < idx < len(points):
-        # interpolating within the range
-        before = points[idx - 1]
-        after = points[idx]
-    elif idx <= 0:
-        # extrapolating behind the range
-        if 2 <= len(points):
-            before, after = points[0], points[1]
-        else:
-            before, after = points[0], points[0]
-    else:
-        # extrapolating beyond the range
-        assert len(points) <= idx
-        if 2 <= len(points):
-            before, after = points[-2], points[-1]
-        else:
-            before, after = points[-1], points[-1]
 
-    if before.time == after.time:
-        weight = 0.0
-    else:
-        weight = (t - before.time) / (after.time - before.time)
-    lat = before.lat + (after.lat - before.lat) * weight
-    lon = before.lon + (after.lon - before.lon) * weight
-    angle = compute_bearing(before.lat, before.lon, after.lat, after.lon)
-    alt: T.Optional[float]
-    if before.alt is not None and after.alt is not None:
-        alt = before.alt + (after.alt - before.alt) * weight
-    else:
-        alt = None
-    return Point(time=t, lat=lat, lon=lon, alt=alt, angle=angle)
+class Interpolator:
+    tracks: T.Sequence[T.Sequence[Point]]
+    track_idx: int
+    # lower bound index in the current track to interpolate timestamps
+    lo: int
+    prev_time: T.Optional[float]
+
+    def __init__(self, tracks: T.Sequence[T.Sequence[Point]]):
+        self.tracks = [track for track in tracks if track]
+        if not self.tracks:
+            raise ValueError("Expect non-empty tracks")
+        self.tracks.sort(key=lambda track: track[0].time)
+        self.track_idx = 0
+        self.lo = 0
+        self.prev_time = None
+
+    @staticmethod
+    def _lsearch_left(
+        track: T.Sequence[Point], t: float, lo: int = 0, hi: T.Optional[int] = None
+    ) -> int:
+        """
+        similar to bisect.bisect_left, but faster in the incremental search case
+        """
+        assert 0 <= lo, "expect non-negative lower bound"
+        if hi is None:
+            hi = len(track)
+        while lo < hi:
+            # assert track[lo - 1].time < t
+            if t <= track[lo].time:
+                break
+            # assert track[lo].time < t
+            lo += 1
+        # assert track[lo - 1] < t <= track[lo]
+        return lo
+
+    def interpolate(self, t: float) -> Point:
+        if self.prev_time is not None:
+            assert self.prev_time <= t, "requires time to be monotonically increasing"
+
+        while self.track_idx < len(self.tracks):
+            track = self.tracks[self.track_idx]
+            if t < track[0].time:
+                return _interpolate_at_index(track, t, 0)
+            elif track[0].time <= t <= track[-1].time:
+                # similar to bisect.bisect_left(points, p, lo=lo) but faster in this case
+                idx = Interpolator._lsearch_left(track, t, lo=self.lo)
+                # set the lower bound to idx - 1
+                # because the next timestamp can still be interpolated in [idx - 1, idx]
+                self.lo = max(idx - 1, 0)
+                return _interpolate_at_index(track, t, idx)
+            self.track_idx += 1
+            self.lo = 0
+
+        interpolated = _interpolate_at_index(self.tracks[-1], t, len(self.tracks[-1]))
+
+        self.prev_time = t
+
+        return interpolated
+
+
+_Point = T.TypeVar("_Point")
+
+
+def sample_points_by_distance(
+    samples: T.Iterable[_Point],
+    min_distance: float,
+    point_func: T.Callable[[_Point], Point],
+) -> T.Generator[_Point, None, None]:
+    prevp: T.Optional[Point] = None
+    for sample in samples:
+        if prevp is None:
+            yield sample
+            prevp = point_func(sample)
+        else:
+            p = point_func(sample)
+            if min_distance < gps_distance((prevp.lat, prevp.lon), (p.lat, p.lon)):
+                yield sample
+                prevp = p
