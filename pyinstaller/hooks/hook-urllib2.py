f790b1aed5280a84b3cc3aebb0993b3f6218079c
Tao Peng <taopeng@meta.com>
2025-01-28 23:48:03 -0800

    remove pyinstaller hooks (#702)

--- pyinstaller/hooks/hook-urllib2.py
+++
@@ -1,5 +0,0 @@
-from PyInstaller.compat import is_darwin
-
-if (is_darwin):
-    import certifi
-    datas = [(certifi.where(), 'lib')]
--- pyinstaller/runtime-hooks/ssl.py
+++
@@ -1,5 +0,0 @@
-import os
-import sys
-
-if sys.platform == 'darwin':
-    os.environ['SSL_CERT_FILE'] = os.path.join(sys._MEIPASS, 'lib', 'cacert.pem')
