90168c160b6fed3431fe80cea739bd4fc6d75ba3
Nikola Pleša <nikolaplesa@fb.com>
2025-04-08 12:39:13 +0200

    Build folder versions for release

---
+++ script/build_win_folder.ps1
@@ -0,0 +1,35 @@
+$OS="win"
+# this is OS arch
+# $ARCH=(wmic OS get OSArchitecture)[2]
+$MAXSIZE32=python3 -c "import sys; print(sys.maxsize <= 2**32)"
+# -ceq case-sensitive equality https://docs.microsoft.com/en-us/powershell/scripting/learn/deep-dives/everything-about-if
+if ($MAXSIZE32 -ceq "True") {
+    $ARCH="32bit"
+} else {
+    $ARCH="64bit"
+}
+
+# build
+mkdir -Force dist
+pyinstaller --version
+pyinstaller --noconfirm --distpath dist\win mapillary_tools_folder.spec
+
+# check
+$FOLDER="dist\win\mapillary_tools"
+$SOURCE="dist\win\mapillary_tools\mapillary_tools.exe"
+dist\win\mapillary_tools\mapillary_tools.exe --version
+$VERSION_OUTPUT=dist\win\mapillary_tools\mapillary_tools.exe --version
+$VERSION=$VERSION_OUTPUT.split(' ')[2]
+$TARGET="dist\releases\mapillary_tools-folder-$VERSION-$OS-$ARCH.zip"
+
+# package
+mkdir -Force dist\releases
+cd dist\win
+Compress-Archive -Path mapillary_tools -DestinationPath ..\..\"$TARGET"
+cd ..\..\
+
+# sha256
+Get-FileHash $TARGET -Algorithm SHA256 | Select-Object Hash > "$TARGET.sha256.txt"
+
+# summary
+Get-ChildItem dist\releases
