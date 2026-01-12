664e42bf9998f4db66a7fab66db4c0c6c2fdcd91
Nikola Pleša <nikolaplesa@fb.com>
2026-01-12 11:16:50 +0100

    Fix license name

--- tests/unit/test_simple_mp4_builder.py
+++ tests/unit/test_simple_mp4_builder.py
@@ -1,6 +1,6 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 #
-# This source code is licensed under the MIT license found in the
+# This source code is licensed under the BSD license found in the
 # LICENSE file in the root directory of this source tree.
 
 import io
