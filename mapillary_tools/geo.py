b4815879fe7e4c046fec1fc9172db7c7bef909ca
Yaroslav Havrylovych <yaroslavh@meta.com>
2026-02-12 18:40:53 +0100

    Add Pyre type checker error suppressions
    Add missing Pyre error code suppressions to three files so they pass
    Pyre type checking without manual modifications:
    
    - construct_mp4_parser.py: suppress error [6] (incompatible parameter
      type) from recursive SwitchMapType type alias usage
    - geo.py: suppress error [16] (undefined attribute) from .sort() on
      Sequence type in Interpolator.__init__
    - telemetry.py: suppress error [16] (undefined attribute) from MRO
      resolution of super().interpolate_with() with multiple inheritance
    
    Comment-only changes, no functional code modifications.

--- mapillary_tools/geo.py
+++ mapillary_tools/geo.py
@@ -3,7 +3,7 @@
 # This source code is licensed under the BSD license found in the
 # LICENSE file in the root directory of this source tree.
 
-# pyre-ignore-all-errors[4]
+# pyre-ignore-all-errors[4, 16]
 from __future__ import annotations
 
 import bisect
